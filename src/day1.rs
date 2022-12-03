8fabb70309fc49c47f3a17662f2e9d2413f93915
Andrew Bell <andrew.bell012276@gmail.com>
2022-12-03 12:21:04 +0000

    pull out common function

--- src/day1.rs
+++ src/day1.rs
@@ -1,8 +1,13 @@
 use crate::utils::{get_file_contents, divide_iterator_on_predicate};
 
-pub fn part_1(file_name: &str) -> String {
+fn get_totals(file_name: &str) -> Vec<u64> {
     let contents = get_file_contents(file_name);
     let entries: Vec<Vec<&str>> = divide_iterator_on_predicate(contents.lines(), |&s| s == "");
     let numbers = entries.iter().map(|v| v.iter().map(|s| s.parse::<u64>().expect("invalid number")));
-    return numbers.map(|v| v.sum::<u64>()).max().unwrap().to_string();
+    return numbers.map(|v| v.sum::<u64>()).collect();
 }
+
+pub fn part_1(file_name: &str) -> String {
+    return get_totals(file_name).iter().max().unwrap().to_string();
+}
+
