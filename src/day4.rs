0be5767c9b8b7fa9c770a855929381e35650073e
Andrew Bell <andrew.bell012276@gmail.com>
2022-12-03 13:07:47 +0000

    skeletons for every day

---
+++ src/day4.rs
@@ -0,0 +1,7 @@
+pub fn part_1(file_name: &str) -> String {
+    unimplemented!("solution not implemented!");
+}
+
+pub fn part_2(file_name: &str) -> String {
+    unimplemented!("solution not implemented!");
+}
