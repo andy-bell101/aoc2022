bcea12f388ac085527f1e50d930ed74e90c036e4
Andrew Bell <andrew.bell012276@gmail.com>
2022-12-04 20:54:18 +0000

    add integration tests for both parts of day 1

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,8 +1,16 @@
 use aoc2022;
 
 #[test]
-fn day_1_integration_test() {
+fn day_1_part_1integration_test() {
     let file_name = "tests/example_files/day1.txt";
     let result = aoc2022::day1::part_1(file_name);
     assert_eq!(result, "24000");
 }
+
+#[test]
+fn day_1_part_2_integration_test() {
+    let file_name = "tests/example_files/day1.txt";
+    let result = aoc2022::day1::part_2(file_name);
+    assert_eq!(result, "45000");
+}
+
