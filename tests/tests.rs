022f84817cbe451972e7bdd6b9a048a7ddfa0bc0
Andrew Bell <andrew.bell012276@gmail.com>
2022-12-04 22:17:27 +0000

    rename tests to make find-replace easier

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,28 +1,28 @@
 use aoc2022;
 
 #[test]
-fn day_1_part_1integration_test() {
+fn day1_part_1_integration_test() {
     let file_name = "tests/example_files/day1.txt";
     let result = aoc2022::day1::part_1(file_name);
     assert_eq!(result, "24000");
 }
 
 #[test]
-fn day_1_part_2_integration_test() {
+fn day1_part_2_integration_test() {
     let file_name = "tests/example_files/day1.txt";
     let result = aoc2022::day1::part_2(file_name);
     assert_eq!(result, "45000");
 }
 
 #[test]
-fn day_2_part_1_integration_test() {
+fn day2_part_1_integration_test() {
     let file_name = "tests/example_files/day2.txt";
     let result = aoc2022::day2::part_1(file_name);
     assert_eq!(result, "15");
 }
 
 #[test]
-fn day_2_part_2_integration_test() {
+fn day2_part_2_integration_test() {
     let file_name = "tests/example_files/day2.txt";
     let result = aoc2022::day2::part_2(file_name);
     assert_eq!(result, "12");
