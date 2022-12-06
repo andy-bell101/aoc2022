61d9acf6df936f03d4d299b6af47b610998c482f
Andrew Bell <andrew.bell012276@gmail.com>
2022-12-06 11:13:34 +0000

    remove annoying warnings for unimplemented solutions

--- src/day12.rs
+++ src/day12.rs
@@ -1,7 +1,7 @@
-pub fn part_1(file_name: &str) -> String {
+pub fn part_1(_file_contents: &str) -> String {
     unimplemented!("solution not implemented!");
 }
 
-pub fn part_2(file_name: &str) -> String {
+pub fn part_2(_file_contents: &str) -> String {
     unimplemented!("solution not implemented!");
 }
