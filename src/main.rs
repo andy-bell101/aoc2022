135cf88077419fe16ef308e36ce8d49668768d68
Andrew Bell <andrew.bell012276@gmail.com>
2022-12-11 21:09:34 +0000

    command line interface

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ extern crate load_file;
 
 mod utils;
 
+use clap::Parser;
+use std::collections::HashMap;
+
 pub mod day1;
 pub mod day2;
 pub mod day3;
@@ -29,27 +32,35 @@ pub mod day22;
 pub mod day23;
 pub mod day24;
 pub mod day25;
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Which day to run. Default is to run all days.
+    #[arg(short, long)]
+    day: Option<u8>,
+    /// Which part to run. Default is to run both parts.
+    #[arg(short, long)]
+    part: Option<u8>,
+}
 
 macro_rules! runner {
     ($($module:tt,)*) => (
-        let mut i = 0;
-        $(
-            i += 1;
-            let file_contents = load_file::load_str!(format!("../input_files/day{}.txt", i).as_str());
-            println!("Day {}, Part 1: {}", i, $module::part_1(file_contents));
-            println!("Day {}, Part 2: {}", i, $module::part_2(file_contents));
-        )*
+        {
+            let mut m = HashMap::new();
+            let mut i: u8 = 0;
+            $(
+                i += 1;
+                let file_contents = load_file::load_str!(format!("../input_files/day{}.txt", i).as_str());
+                m.insert(i, HashMap::from([(1u8, ($module::part_1 as fn(&str) -> String, file_contents)), (2u8, ($module::part_2 as fn(&str) -> String, file_contents))]));
+            )*
+            m
+        }
     )
 }
 
 fn main() {
-    runner!(
-        day1, 
-        day2,
-        day3,
-        day4,
-        day5,
-        day6,
+    let func_map: HashMap<u8, HashMap<u8, (fn(&str) -> String, &str)>> = runner!(
+        day1, day2, day3, day4, day5, day6,
         day7,
         // day8,
         // day9,
@@ -70,4 +81,32 @@ fn main() {
         // day24,
         // day25,
     );
+    let args = Args::parse();
+
+    let days = match args.day {
+        Some(x) => {
+            if !func_map.contains_key(&x) {
+                panic!("Invalid day input. Solution may not be implemented yet");
+            }
+            vec![x]
+        }
+        None => (1..=func_map.len() as u8).collect(),
+    };
+    let parts = match args.part {
+        Some(x) => {
+            if x == 1 || x == 2 {
+                vec![x]
+            } else {
+                panic!("Invalid part input");
+            }
+        }
+        None => vec![1, 2],
+    };
+
+    for i in days.iter() {
+        for j in parts.iter() {
+            let (func, contents) = func_map[i][j];
+            println!("Day {} part {} solution: {}", i, j, func(contents));
+        }
+    }
 }
